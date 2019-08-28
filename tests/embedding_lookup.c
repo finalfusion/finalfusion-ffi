1be458441c9db6af44487f024c47eddba7b365c4
Sebastian Puetz <sebastian.puetz@student.uni-tuebingen.de>
2019-08-28 23:09:23 +0200

    Add embedding lookup and dimensionality query.
    Add methods to lookup embeddings and query for the dimensionality
    of the embeddings. And a test.

---
+++ tests/embedding_lookup.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+
+#include <finalfusion.h>
+
+int main() {
+  float *embedding;
+  ff_embeddings embeds = ff_read_embeddings("data/test.fifu");
+  if (embeds == NULL) {
+    return 1;
+  }
+
+  /* in vocab*/
+  embedding = ff_embedding_lookup(embeds, "Berlin");
+  if (embedding == NULL) {
+    return 1;
+  }
+  free(embedding);
+
+  /* oov */
+  embedding = ff_embedding_lookup(embeds, "Tübingen");
+  if (embedding != NULL) {
+    return 1;
+  }
+  free(embedding);
+
+  ff_free_embeddings(embeds);
+  return 0;
+}
